de87de3c4d4ef8362d2216f7f9cb79c38ecfe85f
jeremylzn <lznjeremy@gmail.com>
2021-09-28 21:30:57 +0300

    add article list and article filter

--- pages/components/articles-list.tsx
+++ pages/components/articles-list.tsx
@@ -1,32 +1,60 @@
 import type { NextPage } from 'next'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { faUserEdit } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import styles from '../../styles/ArticlesList.module.css'
+import React, { Component } from 'react';
 
-export const ArticleList = (data: any) => {
+class ArticleList extends Component<{}, { allArticles: any[], filteredArticles: any[] }> {
+
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      allArticles: props.articles,
+      filteredArticles: props.articles,
+    };
+  }
+
+  public handleSearch = (event: any) => {
+    let value = event.target.value.toLowerCase();
+    let result = [];
+    result = this.state.allArticles.filter((data) => {
+      return (data.title.toLowerCase()).search(value) != -1;
+    });
+    this.setState({ filteredArticles: result });
+  }
+
+  render() {
     return (
-        <div className="container">
-      {
-        (data.props) ? 
-        data.props.pageProps.posts.map((el:any) =>
-          <div className="row">
-            <div className="card mb-3">
+      <div className="container">
+
+        <div className="input-group flex-nowrap mt-5 mb-5">
+          <span className="input-group-text" id="addon-wrapping">Search</span>
+          <input type="text" className="form-control" placeholder="Search ..." onChange={(event) => this.handleSearch(event)} aria-label="Search" aria-describedby="addon-wrapping"/>
+        </div>
+
+        {this.state.filteredArticles.map((el: any, index: number) =>
+          <div className="row" key={index}>
+            <div className={styles.card + " mb-3"}>
               <div className="row g-0">
                 <div className="col-md-4">
                   <img src={el.featuredImage.node.sourceUrl} className="img-fluid rounded-start" alt={el.featuredImage.node.altText} />
                 </div>
                 <div className="col-md-8">
                   <div className="card-body">
-                    <h5 className="card-title">{el.title}</h5>
-                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                    <h5 className="card-title text-center">{el.title}</h5>
+                    <p className="card-text mt-5"><FontAwesomeIcon icon={faUserEdit} /> : {el.author.node.name}</p>
+                    <p className="card-text mt-5"><small className="text-muted">{new Date(el.date).toLocaleDateString()}</small></p>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        ) : <div className="container">
-          <h1>THERE IS NO ARTICLES</h1>
-        </div>
-      }
-    </div>
+        )
+        }
+      </div>
     )
+  }
 }
+
+export default ArticleList;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,37 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import '../styles/Home.module.css'
+import ArticleList from './components/articles-list'
 
 import React, { useState, useEffect } from 'react';
 
 const Home: NextPage = () => {
-  const [articles, setArticlesData]: any = useState({})
+  const [articles, setArticlesData]: any = useState(null)
+  const [categories, filterCategoriesData]: any = useState(null)
+
 
   // useEffect to call the API once mounted and set the data
   useEffect(() => {
-    fetch('/api/data', {
-      method: 'get',
-      headers: {
-        'content-type': 'application/json',
-      }
-    })
+    fetch('/api/data')
       .then((res) => res.json())
       .then((articles) => {
-        setArticlesData(articles)
+        setArticlesData(articles.props.pageProps.posts)
+
+        var filteredCateg = articles.props.pageProps.posts.reduce((unique: any, o: any) => {
+          if (!unique.some((obj: any) => obj.categories.nodes[0].name === o.categories.nodes[0].name && obj.categories.nodes[0].uri === o.categories.nodes[0].uri)) {
+            unique.push(o);
+          }
+          return unique;
+        }, []);
+
+        filterCategoriesData(filteredCateg)
+
       })
   }, []);
 
   return (
     <div>
+      {articles && <ArticleList articles={articles} />}
     </div>
   )
 }
